107dd67a4440cc44fb630d7998605cfd1e9cef1b
VadimGrznk <vg@atticlab.net>
2021-02-26 19:29:42 +0200

    Add track data parameters to example instruction

--- create_and_verify/src/instruction.rs
+++ create_and_verify/src/instruction.rs
@@ -1,6 +1,7 @@
 //! Instruction types
 
-use crate::error::ProgramTemplateError;
+use crate::state::TrackData;
+use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     instruction::{AccountMeta, Instruction},
     program_error::ProgramError,
@@ -8,9 +9,21 @@ use solana_program::{
     sysvar,
 };
 
+/// Instruction arguments
+#[repr(C)]
+#[derive(BorshSerialize, BorshDeserialize, Clone)]
+pub struct InstructionArgs {
+    /// data of track
+    pub track_data: TrackData,
+    /// signature to verify
+    pub signature: [u8; audius::state::SecpSignatureOffsets::SECP_SIGNATURE_SIZE],
+    /// recovery ID used to verify signature
+    pub recovery_id: u8,
+}
+
 /// Instruction definition
 #[repr(C)]
-#[derive(Clone)]
+#[derive(BorshSerialize, BorshDeserialize, Clone)]
 pub enum TemplateInstruction {
     ///   Example
     ///
@@ -18,43 +31,7 @@ pub enum TemplateInstruction {
     ///   2. [] Signer group
     ///   3. [] Audius program account
     ///   4. [] Sysvar instruction account
-    ExampleInstruction(audius::instruction::SignatureData),
-}
-impl TemplateInstruction {
-    /// Unpacks
-    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
-        let (&tag, rest) = input
-            .split_first()
-            .ok_or(ProgramTemplateError::ExampleError)?;
-        Ok(match tag {
-            0 => {
-                let mut signature: [u8; 64] = [0u8; 64];
-                signature.copy_from_slice(&rest[0..64]);
-                let signature_data = audius::instruction::SignatureData {
-                    signature,
-                    recovery_id: rest[64],
-                    message: rest[64 + 1..].to_vec(),
-                };
-                Self::ExampleInstruction(signature_data)
-            }
-            _ => return Err(ProgramTemplateError::ExampleError.into()),
-        })
-    }
-
-    /// Packs
-    pub fn pack(&self) -> Vec<u8> {
-        let mut buf;
-        match self {
-            Self::ExampleInstruction(signature_data) => {
-                buf = vec![];
-                buf.push(0);
-                buf.extend_from_slice(&signature_data.signature);
-                buf.push(signature_data.recovery_id);
-                buf.extend_from_slice(&signature_data.message);
-            }
-        };
-        buf
-    }
+    ExampleInstruction(InstructionArgs),
 }
 
 /// Create `Example` instruction
@@ -62,10 +39,12 @@ pub fn init(
     program_id: &Pubkey,
     valid_signer_account: &Pubkey,
     signer_group: &Pubkey,
-    signature_data: audius::instruction::SignatureData,
+    track_data: InstructionArgs,
 ) -> Result<Instruction, ProgramError> {
-    let init_data = TemplateInstruction::ExampleInstruction(signature_data);
-    let data = init_data.pack();
+    let init_data = TemplateInstruction::ExampleInstruction(track_data);
+    let data = init_data
+        .try_to_vec()
+        .or(Err(ProgramError::InvalidArgument))?;
     let accounts = vec![
         AccountMeta::new_readonly(*valid_signer_account, false),
         AccountMeta::new_readonly(*signer_group, false),
