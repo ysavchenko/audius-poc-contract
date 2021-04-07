c8b2397535e56f5dcdacdb03f7a4ca3af7c58fae
VadimGrznk <vg@atticlab.net>
2021-04-07 17:00:57 +0300

    Add Borsh to program

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -15,5 +15,5 @@ pub mod entrypoint;
 
 // Export current sdk types for downstream users building with a different sdk version
 pub use solana_program;
-
-solana_program::declare_id!("3QqhXLvBgPZ4DCV3YjyzpiQWfeR4Lf2bSKqSnj5c8wkE");
+// 3QqhXLvBgPZ4DCV3YjyzpiQWfeR4Lf2bSKqSnj5c8wkE
+solana_program::declare_id!("77xRWv8Z3kaQpD9K3Den7YWJ7sxsf1KTnw5MdcM7Gtnw");
